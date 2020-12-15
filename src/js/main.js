3d79c7711c1d425de63e40b9d9033cbfb602ebb3
DESKTOP-7UQJTRQ\Quy_cut3 <phamhoangquy99vn@gmail.com>
2020-12-15 09:42:14 +0700

    Add code

---
+++ src/js/main.js
@@ -0,0 +1,116 @@
+$(document).ready(function() {
+    swiperInit();
+    toggleMobileMenu();
+    mappingMenu();
+});
+
+function toggleMobileMenu() {
+    $('.header_btn').click(function() {
+        $(this).toggleClass("click");
+        $('.menu_mobile').toggleClass("show");
+        $('.overlay').toggleClass("show");
+    });
+    $('.overlay').click(function() {
+        $(this).toggleClass("click");
+        $('.menu_mobile').toggleClass("show");
+        $('.overlay').toggleClass("show");
+    });
+    $('.search-icon').click(function() {
+        $(this).toggleClass("click");
+        $('.block_search').toggleClass("show");
+    });
+}
+
+function mappingMenu() {
+    return new MappingListener({
+        selector: ".nav-menu",
+        mobileWrapper: ".mobile-wrapper",
+        mobileMethod: "appendTo",
+        desktopWrapper: ".header_menu",
+        desktopMethod: "prependTo",
+        breakpoint: 1281
+    }).watch();
+}
+
+
+function swiperInit() {
+    var homerSwiper = new Swiper(".home-banner .swiper-container", {
+        speed: 1205,
+        slidesPerView: 1,
+        autoplay: {
+            delay: 3000
+        },
+    });
+
+    var brandSwiper = new Swiper(".home_s-5 .swiper-container", {
+        // Optional parameters
+        speed: 1000,
+        autoplay: {
+            delay: 2000
+        },
+        navigation: {
+            nextEl: '.home_s-5 .nav-arrow-next',
+            prevEl: '.home_s-5 .nav-arrow-prev',
+        },
+        spaceBetween: 30,
+        breakpointsInverse: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            400: {
+                slidesPerView: 1,
+
+            },
+            480: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            1025: {
+                slidesPerView: 5,
+            },
+            1440: {
+                slidesPerView: 6,
+            },
+        },
+
+    });
+
+    var brandSwiper = new Swiper(".home_s-7 .swiper-container", {
+        // Optional parameters
+        speed: 1000,
+        autoplay: {
+            delay: 2000
+        },
+        navigation: {
+            nextEl: '.home_s-7 .nav-arrow-next',
+            prevEl: '.home_s-7 .nav-arrow-prev',
+        },
+        spaceBetween: 80,
+        breakpointsInverse: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            400: {
+                slidesPerView: 1,
+
+            },
+            480: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            1025: {
+                slidesPerView: 4,
+            },
+            1440: {
+                slidesPerView: 4,
+            },
+        },
+
+    });
+}
